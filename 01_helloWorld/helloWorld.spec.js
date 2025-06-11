3053cabb89373f9b204dfb5ed81629b9a928543f
zegraa <odinproject65@gmail.com>
2025-06-11 16:38:19 -0500

    Update helloWorld.spec.js

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,4 +1,4 @@
-const helloWorld = require('./helloWorld');
+const helloWorld = require('./helloWorld-solution');
 
 describe('Hello World', function() {
   test('says "Hello, World!"', function() {
