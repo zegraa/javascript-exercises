fc3b8a261d4c1407245e358b29d8bb0ed1e91a25
zegraa <odinproject65@gmail.com>
2025-06-15 23:34:16 -0500

    Update joinStrings.js
    changed everyhing to example code

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -19,13 +19,10 @@ const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am 
 
 console.log(greeting);
 
-// Do not change this
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName
 }
