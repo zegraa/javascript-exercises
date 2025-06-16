810fb610325538f069827b02ce3e1c8d66c358d0
zegraa <odinproject65@gmail.com>
2025-06-15 23:22:55 -0500

    Update joinStrings.js
    Added ";" at the ends

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,10 +9,10 @@
 
 // Add your code right below, good luck!
 
-const birthYear = 1947
-const thisYear = 1965
-const firstName = Carlos
-const lastName = Stevenson
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = Carlos;
+const lastName = Stevenson;
 const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " +(thisYear - birthYear) + "years old.";
 
 console.log(greeting);
