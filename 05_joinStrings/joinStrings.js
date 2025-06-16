bbaf129c695849267ec365290f793f90822f4ca9
zegraa <odinproject65@gmail.com>
2025-06-15 23:21:17 -0500

    Update joinStrings.js
    Added final console.log information

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,9 +9,13 @@
 
 // Add your code right below, good luck!
 
+const birthYear = 1947
+const thisYear = 1965
+const firstName = Carlos
+const lastName = Stevenson
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " +(thisYear - birthYear) + "years old.";
 
-
-
+console.log(greeting);
 
 // Do not change this
 module.exports = {
